1f9d7f01a679fa5ef88279e2e79a64a0380bafa6
chabane <chabane.moha97@gmail.com>
2021-01-24 21:08:45 +0100

    refactor : deplacement de la page accueil vers son propre composant

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
+import { EventComponent } from './event/event.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"evenements",component:EventComponent},
+  {path:"", component:HomeComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
