2c9fd2a6c9cd577a2c1893e7c11cba73ddbe1be8
chabane <chabane.moha97@gmail.com>
2021-01-25 23:38:03 +0100

    refactor : ajutement de la page evenement

--- src/app/event.service.ts
+++ src/app/event.service.ts
@@ -1,15 +1,29 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { catchError } from 'rxjs/operators';
+import { HttpHeaders } from '@angular/common/http';
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Access-Control-Allow-Origin':  'https://agentu.herokuapp.com/',
+    'Access-Control-Allow-Methods':  'GET, POST, PATCH, PUT, DELETE, OPTIONS',
+    'Access-Control-Allow-Headers':  'Origin, Content-Type, X-Auth-Token',
+    'Access-Control-Allow-Credentials':  'true',
+    'Content-Type':  'application/json',
+    'Authorization': 'Basic ' + btoa('agentu:AgentU@2020')
+  })
+};
 
 @Injectable({
   providedIn: 'root'
 })
 export class EventService {
 
-  constructor(private http:HttpClient) { }
+  constructor(private http:HttpClient) { 
+    
+  }
 
   public getEvents(){
+   // this.http.get("https://agentu.herokuapp.com/evenements/" ,{withCredentials: true })
     return this.http.get("https://agentu.herokuapp.com/evenements/");
  
   }
