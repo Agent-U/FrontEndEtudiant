e6fb552b84a613d97e295db277f1d0202495e7ca
chabane <chabane.moha97@gmail.com>
2021-01-26 12:20:19 +0100

    feat : création du modal qui permet d'afficher des détails sur l'étudiant

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
-import { EventComponent } from './event/event.component';
 import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
-  {path:"evenements",component:EventComponent},
-  {path:"", component:HomeComponent}
+  {
+  path:"", component:HomeComponent}
 ];
 
 @NgModule({
