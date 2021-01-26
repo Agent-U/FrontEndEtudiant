031a813243aae809bd47f2a011eb251774ac9ab2
arab <nassimarab@gmail.com>
2021-01-26 21:55:41 +0100

    feat: création du service des incidents

---
+++ src/app/serviceIncidents/incidents.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Incident } from '../incident';
+
+const optionRequete = {
+  headers: new HttpHeaders({ 
+    'Access-Control-Allow-Origin':'*',
+    'Access-Control-Allow-Methods':'POST, GET',
+    'Access-Control-Allow-Headers':'X-PINGOTHER, Content-Type'
+  })
+};
+
+@Injectable({
+  providedIn: 'root'
+})
+export class IncidentsService {
+
+
+  
+
+  constructor(private http:HttpClient) { }
+
+
+}
